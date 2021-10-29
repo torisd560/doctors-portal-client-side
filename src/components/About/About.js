023d7b90adbe6cc492dcfc9a6c771467c0851c11
torisd560 <mitalisd123@gmail.com>
2021-10-30 04:04:49 +0600

    added about all updates

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Button, Col, Container } from 'react-bootstrap';
 import aboutBanner from '../../images/aboutBanner.png'
 import './About.css'
+import agent1 from '../../images/agent1.jpg'
+import agent2 from '../../images/agent2.jpg'
+import agent3 from '../../images/agent3.jpg'
 const About = () => {
     return (
         <div >
@@ -41,6 +44,40 @@ const About = () => {
                    <p className = 'mt-4'> Travel dates back to antiquity where wealthy Greeks and Romans would travel for leisure to their summer homes and villas in cities such as Pompeii and Baiae. While early travel tended to be slower, more dangerous, and more dominated by trade and migration.</p>
                </div>
            </div>
+           {/* ================travel agent======================= */}
+           <section className = 'travel-container text-secondary text-center '>
+               <h2 className = 'custom-text-primary fw-bold'>Our Travel Agents</h2>
+               <p className = 'lh-lg my-4'>While early travel tended to be slower, more dangerous, and more dominated by trade and migration Travel dates back to antiquity where wealthy Greeks and Romans would travel for leisure to their summer homes and villas in cities such as Pompeii and Baiae</p>
+           <div className="row my-5 ">
+               <div className="col-md-4">
+                   <div>
+                       <img src={agent1} alt=""  className='img-fluid' />
+                      <div className = 'p-3 shadow-lg rounded-3'>
+                      <h5 className = 'custom-text-primary fw-bold'>Rubie Talley</h5>
+                       <p>Tour Guide</p>
+                      </div>
+                   </div>
+               </div>
+               <div className="col-md-4">
+                   <div>
+                       <img src={agent2} alt=""  className='img-fluid' />
+                       <div className = ' p-3 shadow-lg rounded-3'>
+                       <h5 className = 'custom-text-primary fw-bold'>Marcel Frame</h5>
+                       <p>Tour Guide</p>
+                       </div>
+                   </div>
+               </div>
+               <div className="col-md-4">
+                   <div>
+                       <img src={agent3} alt=""  className='img-fluid' />
+                       <div className = ' p-3 shadow-lg rounded-3'>
+                       <h5 className = 'custom-text-primary fw-bold'>Dina Driscoll</h5>
+                       <p>Tour Guide</p>
+                       </div>
+                   </div>
+               </div>
+           </div>
+           </section>
            </Container>
            
         </div>
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import { Col } from 'react-bootstrap';
 
 const Contact = () => {
     return (
         <div>
-           <h1>This is Contact</h1> 
+           <Col className='page-top-banner text-white fw-bold px-5'>
+               <div className = 'text-center p-5'>
+               <h1 >Contact Us</h1>
+                <p><i className="fas fa-map-marker-alt me-2"></i>Home / Contact</p>
+               </div>
+            </Col>
         </div>
     );
 };
